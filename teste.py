78ee638a7605325fadf02e2d02122b1b72ef5ff7
Elisson Douglas <elisson2929gdt@gmail.com>
2022-11-30 21:02:40 -0300

    second commit

--- teste.py
+++
@@ -1,8 +0,0 @@
-import os
-
-lista = os.listdir('/home/elisson/Downloads/Outros/')
-for arquivo in lista:
-    if os.path.isdir(f'/home/elisson/Downloads/Outros/{arquivo}'):
-        print('sim')
-    else:
-        print('nao')
